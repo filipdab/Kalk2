7f91151cec7f9a6caec7ad7fc1054e1303919c68
filipdab <filipdab@outlook.com>
2022-04-23 13:26:07 +0200

    ver 0.0.3 dodane przyciski i oprogramowany btn zamykajacy program

--- kalkulator.py
+++ kalkulator.py
@@ -1,30 +1,61 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QLabel, QGridLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QGridLayout, QLineEdit, QPushButton, QHBoxLayout
 
 class Kalkulator(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.interfejs()
-
+    def koniec(self):
+        self.close()
     def interfejs(self):
 
         etykieta1 = QLabel("Liczba 1:", self)
         etykieta2 = QLabel("Liczba 2:", self)
         etykieta3 = QLabel("Wynik:", self)
+        self.liczba1Edt = QLineEdit()
+        self.liczba2Edt = QLineEdit()
+        self.wynikEdt = QLineEdit()
+        dodajBtn = QPushButton("&Dodaj", self)
+        odejmijBtn = QPushButton("&Odejmij", self)
+        dzielBtn = QPushButton("&Dziel", self)
+        mnozBtn = QPushButton("&Mnóż", self)
+        koniecBtn = QPushButton("&Koniec", self)
+        koniecBtn.resize(koniecBtn.sizeHint())
 
+        self.wynikEdt.readonly = True
+        self.wynikEdt.setToolTip('Wpisz <b>liczby</b> i wybierz działanie...')
         #układ
         ukladT = QGridLayout()
         ukladT.addWidget(etykieta1, 0, 0)
         ukladT.addWidget(etykieta2, 0, 1)
         ukladT.addWidget(etykieta3, 0, 2)
 
+        ukladT.addWidget(self.liczba1Edt, 1, 0)
+        ukladT.addWidget(self.liczba2Edt, 1, 1)
+        ukladT.addWidget(self.wynikEdt, 1, 2)
+
+        ukladH = QHBoxLayout()
+        ukladH.addWidget(dodajBtn)
+        ukladH.addWidget(odejmijBtn)
+        ukladH.addWidget(dzielBtn)
+        ukladH.addWidget(mnozBtn)
+        ukladH.addWidget(dodajBtn)
+
+        ukladT.addLayout(ukladH, 2, 0, 1, 3)
+        ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
+
+
+
         #przypisanie ukladu do okna
         self.setLayout(ukladT)
+        koniecBtn.clicked.connect(self.koniec)
+
+        self.setGeometry(20, 20, 300, 100)
 
         self.resize(300, 100)
-        self.setWindowTitle("Kalkulatorek")
+        self.setWindowTitle("Kalkulatorek v. 0.0.3")
         self.show()
 
 
@@ -33,4 +64,5 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     okno = Kalkulator()
+
     sys.exit(app.exec())
