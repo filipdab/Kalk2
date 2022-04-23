874d0094e459302e83a2be24db2efaad1faba3e7
filipdab <filipdab@outlook.com>
2022-04-23 10:23:22 +0200

    ver 0.0.1

---
+++ kalkulator.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class Kalkulator(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.interfejs()
+
+    def interfejs(self):
+        self.resize(300, 100)
+        self.setWindowTitle("Kalkulatorek")
+        self.show()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    okno = Kalkulator()
+    sys.exit(app.exec())
