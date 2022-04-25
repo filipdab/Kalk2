8e21a626a315170f8c19daeefc00615e5d18cf7c
filipdab <filipdab@outlook.com>
2022-04-25 08:36:08 +0200

    ver 0.0.4 final - simple calculator in Python QT

--- kalkulator.py
+++ kalkulator.py
@@ -2,15 +2,61 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QLabel, QGridLayout, QLineEdit, QPushButton, QHBoxLayout
 
+
 class Kalkulator(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.interfejs()
+
     def koniec(self):
         self.close()
-    def interfejs(self):
 
+    def closeEvent(self, event):
+
+        odp = QMessageBox.question(
+            self, 'Komunikat',
+            "Czy na pewno koniec?",
+            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if odp == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dzialanie(self):
+
+        nadawca = self.sender()
+
+        try:
+            liczba1 = float(self.liczba1Edt.text())
+            liczba2 = float(self.liczba2Edt.text())
+            wynik = ""
+
+            if nadawca.text() == "&Dodaj":
+                wynik = liczba1 + liczba2
+            elif nadawca.text() == "&Odejmij":
+                wynik = liczba1 - liczba2
+            elif nadawca.text() == "&Mnóż":
+                wynik = liczba1 * liczba2
+
+            else: # dzielenie
+                try:
+                    wynik = round(liczba1 / liczba2, 9)
+                except ZeroDivisionError:
+                    QMessageBox.critical(
+                      self, "Błąd", "Nie można dzielić przez zero"
+                    )
+                    return
+            self.wynikEdt.setText(str(wynik))
+
+        except ValueError:
+            QMessageBox.warning(self, "Błąd", "Błędne dane", QMessageBox.Ok)
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
+    def interfejs(self):
         etykieta1 = QLabel("Liczba 1:", self)
         etykieta2 = QLabel("Liczba 2:", self)
         etykieta3 = QLabel("Wynik:", self)
@@ -26,7 +72,7 @@ class Kalkulator(QWidget):
 
         self.wynikEdt.readonly = True
         self.wynikEdt.setToolTip('Wpisz <b>liczby</b> i wybierz działanie...')
-        #układ
+        # układ
         ukladT = QGridLayout()
         ukladT.addWidget(etykieta1, 0, 0)
         ukladT.addWidget(etykieta2, 0, 1)
@@ -46,16 +92,19 @@ class Kalkulator(QWidget):
         ukladT.addLayout(ukladH, 2, 0, 1, 3)
         ukladT.addWidget(koniecBtn, 3, 0, 1, 3)
 
-
-
-        #przypisanie ukladu do okna
+        # przypisanie ukladu do okna
         self.setLayout(ukladT)
         koniecBtn.clicked.connect(self.koniec)
 
-        self.setGeometry(20, 20, 300, 100)
+        dodajBtn.clicked.connect(self.dzialanie)
+        odejmijBtn.clicked.connect(self.dzialanie)
+        mnozBtn.clicked.connect(self.dzialanie)
+        dzielBtn.clicked.connect(self.dzialanie)
 
+        self.setGeometry(20, 20, 300, 100)
+        self.liczba1Edt.setFocus()
         self.resize(300, 100)
-        self.setWindowTitle("Kalkulatorek v. 0.0.3")
+        self.setWindowTitle("Kalkulatorek v. 0.0.4")
         self.show()
 
 
